1b8d2ec832c32e5996897c7a71e6f1713405e429
EduardoArenasI <eduardoarenas014@gmail.com>
2022-05-18 12:34:48 -0500

    master

--- src/app/shared/interfaces.ts
+++ src/app/shared/interfaces.ts
@@ -1,23 +1,36 @@
+
 export interface Model {
     id: string;
 }
-export interface ListWCursor {
+
+export interface ListWCursor<T> {
 
 }
-export interface FilterParams {
-
-}
-export interface SortOrder {
+export interface FilterParams<T> {
 
 }
 
-export interface Range {
+export interface SortOrder<T> {
+    field: any;
+    order: string;
 
 }
-export interface WhereIn {
+
+export interface Range<T> {
+    nonInclusive: any;
+    field: any;
+    start: number;
+    end: number;
+
+}
+export interface WhereIn<T> {
+    values: any;
+    field: any;
 
 }
 
-export interface WhereContains {
+export interface WhereContains<T> {
+    values: any;
+    field: any;
 
 }
