21a0227fc727826035d4d3553c5615a4433e9e63
Eduardo Emmanuel Arenas Interian <eduardoarenas014@gmail.com>
2022-06-18 12:48:32 -0500

    master

--- src/app/components/show/show.component.ts
+++ src/app/components/show/show.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+
+
 import { Post } from 'src/app/post.model';
 import { PostService } from 'src/app/post.service';
 import { ApiService } from 'src/app/shared/api.service';
@@ -14,16 +16,11 @@ import { ApiService } from 'src/app/shared/api.service';
 })
 
 
-
 export class ShowComponent implements OnInit {
-
   Post: Post[]
   constructor(private postService: PostService) { }
-
   ngOnInit(): void {
-
   }
-
 
   deleteRow = (post: string) => { this.postService.destroy(post) }
 } 
